b6213f50831aa113f53c65128000c29a60d90c19
NahoMer <nahomer.tech@gmail.com>
2025-07-05 11:32:25 -0300

    Update views.py

--- users/views.py
+++ users/views.py
@@ -18,21 +18,3 @@ class EmailTokenObtainPairView(TokenObtainPairView):
 class RegisterView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-class LoginView(APIView):
-    permission_classes = [AllowAny]
-    
-    def post(self, request):
-        email = request.data.get('email')
-        password = request.data.get('password')
-        user = authenticate(request, email=email, password=password)
-        
-        if user:
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({
-                'message': 'Login successful',
-                'token': token.key,
-                'user': UserSerializer(user).data
-            }, status=status.HTTP_200_OK)
-        
-        return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
