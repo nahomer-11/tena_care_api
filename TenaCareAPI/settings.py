3ca5fb102fc1c23cd3a39855a0f05c7ec896a6d4
NahoMer <nahomer.tech@gmail.com>
2025-07-05 11:13:19 -0300

    Update settings.py

--- TenaCareAPI/settings.py
+++ TenaCareAPI/settings.py
@@ -30,9 +30,15 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),  # token valid 1 day
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
 AUTHENTICATION_BACKENDS = [
