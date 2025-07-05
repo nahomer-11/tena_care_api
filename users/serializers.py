6175a38622e27cf079f3f680b2f9baed3256b6e2
NahoMer <nahomer.tech@gmail.com>
2025-07-05 06:31:32 -0300

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -5,7 +5,7 @@ from rest_framework.exceptions import ValidationError
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'full_name', 'phone_number', 'is_active', 'is_staff']
+        fields = ['id', 'email', 'full_name', 'phone_number', 'is_active', 'password', 'is_staff']
         read_only_fields = ['id', 'is_active', 'is_staff']
 
     def validate_email(self, value):
@@ -17,3 +17,10 @@ class UserSerializer(serializers.ModelSerializer):
         if not value:
             raise serializers.ValidationError("Full name is required.")
         return value
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # Password will be hashed
+        user.save()
+        return user
