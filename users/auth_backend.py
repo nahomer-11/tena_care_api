55a60765773bca9a7f82de0ed255ca9913201428
NahoMer <nahomer.tech@gmail.com>
2025-07-05 11:29:52 -0300

    Update auth_backend.py

--- users/auth_backend.py
+++ users/auth_backend.py
@@ -5,10 +5,12 @@ UserModel = get_user_model()
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
-        email = kwargs.get('email', username)
+        email = kwargs.get('email') or username
+        if email is None or password is None:
+            return None
         try:
             user = UserModel.objects.get(email=email)
-            if user.check_password(password):
+            if user.check_password(password) and user.is_active:
                 return user
         except UserModel.DoesNotExist:
             return None
