bfe2d530ec871bbfe9b60ab3726b1fc7a64378fc
NahoMer <nahomer.tech@gmail.com>
2025-07-24 11:06:16 -0300

    Update urls.py

--- tenaCare_agent/urls.py
+++ tenaCare_agent/urls.py
@@ -4,7 +4,8 @@ from .views import (
     ChatSessionListView,
     CreateChatSessionView,
     MessageListView,
-    SendMessageView
+    SendMessageView,
+    ClearChatHistoryView
 )
 
 urlpatterns = [
@@ -12,4 +13,5 @@ urlpatterns = [
     path('sessions/create/', CreateChatSessionView.as_view()),
     path('sessions/<int:session_id>/messages/', MessageListView.as_view()),
     path('sessions/<int:session_id>/send/', SendMessageView.as_view()),
+    path('sessions/clear-history/', ClearChatHistoryView.as_view(), name='clear_chat_history'),
 ]
